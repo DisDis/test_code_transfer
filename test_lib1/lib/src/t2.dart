b9c12a667cd70a10a92a080dbbf374305b8184d1
Igor Demyanov <igor.demyanov@gmail.com>
2018-09-20 02:54:49 +0300

    added double import change

--- test_lib1/lib/src/t2.dart
+++ test_lib1/lib/src/t2.dart
@@ -1,6 +1,8 @@
+import 'package:code_health_meta/code_health_meta.dart';
 import 'package:test_lib1/src/t1.dart';
 import 'package:test_lib1/src/t1.dart' as lib1T1;
 
+@CHTransfer(dest:'src/new_lib1_t2.dart')
 class T2 extends T1{
 
 }
---
+++ test_lib1/lib/src/t3.dart
@@ -0,0 +1,10 @@
+import 'package:test_lib1/src/t1.dart';
+import 'package:test_lib1/src/t2.dart';
+
+class T3_2 extends T2{
+
+}
+
+class T3_1 extends T1{
+
+}
