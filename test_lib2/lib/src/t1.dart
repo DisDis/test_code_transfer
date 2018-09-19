71353c6bce01c27b5840e7f722d1eed8b2243687
Igor Demyanov <igor.demyanov@gmail.com>
2018-09-20 00:32:31 +0300

    added part files

--- test_lib2/lib/src/t1.dart
+++ test_lib2/lib/src/t1.dart
@@ -1,4 +1,7 @@
+library lib2_t1;
 import 'package:code_health_meta/code_health_meta.dart';
+
+part 't1.g.dart';
 
 @CHTransfer(dest:'src/new_lib2_t1.dart')
 class T1{
---
+++ test_lib2/lib/src/t1.g.dart
@@ -0,0 +1,5 @@
+part of lib2_t1;
+
+class T1_Part{
+
+}
