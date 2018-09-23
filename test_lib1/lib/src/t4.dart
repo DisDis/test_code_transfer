d5b703d1c4187fdb184b605963173780f4382198
Igor Demyanov <igor.demyanov@gmail.com>
2018-09-24 01:44:10 +0300

    more cases

--- test_lib1/lib/src/t4.dart
+++ test_lib1/lib/src/t4.dart
@@ -1,6 +1,11 @@
 import 'package:code_health_meta/code_health_meta.dart';
+import 'package:test_lib1/src/t3.dart';
 
 @CHTransfer(package: 'new_test_lib1', dest:'src/new_lib1_t4.dart')
 class T4{
+
+}
+
+class T4_LinkT3 extends T3_1{
 
 }
