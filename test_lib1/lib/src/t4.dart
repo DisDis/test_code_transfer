4a8f78054c71dcf4826d32726d04ee77a915f2f6
Igor Demyanov <igor.demyanov@gmail.com>
2018-09-25 02:15:57 +0300

    more cases

--- test_lib1/lib/src/t4.dart
+++ test_lib1/lib/src/t4.dart
@@ -1,7 +1,7 @@
 import 'package:code_health_meta/code_health_meta.dart';
 import 'package:test_lib1/src/t3.dart';
 
-@CHTransfer(package: 'new_test_lib1', dest:'src/new_lib1_t4.dart')
+@CHTransfer(package: 'new_test_lib1', filename:'new_lib1_t4.dart' , directory: 'src/1/')
 class T4{
 
 }
